7bbfb10369bdeb1eeee44825e8c97ba415f26911
roni <ronirensburg@gmail.com>
2021-06-26 21:13:32 +0300

    add configure to nodeMalier

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 var transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  ignoreTLS: false,
+  secure: false,
   auth: {
     user: "ronirensburg@gmail.com",
     pass: "Azsxsza321",
